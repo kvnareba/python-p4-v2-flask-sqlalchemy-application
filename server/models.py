36beca02b94057f0ad9d4cf3962fd54e57b61a30
kvn <kvnareba@gmail.com>
2024-07-22 20:47:48 +0300

    com

--- server/models.py
+++ server/models.py
@@ -1,21 +1,54 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+#!/usr/bin/env python3
 
-# contains definitions of tables and associated schema constructs
-metadata = MetaData()
+from flask import Flask, make_response
+from flask_migrate import Migrate
 
-# create the Flask SQLAlchemy extension
-db = SQLAlchemy(metadata=metadata)
+from models import db, Pet
 
-# define a model class by inheriting from db.Model.
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
 
 
-class Pet(db.Model):
-    __tablename__ = 'pets'
+@app.route('/')
+def index():
+    response = make_response(
+        '<h1>Welcome to the pet directory!</h1>',
+        200
+    )
+    return response
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    species = db.Column(db.String)
 
-    def __repr__(self):
-        return f'<Pet {self.id}, {self.name}, {self.species}>'
+@app.route('/pets/<int:id>')
+def pet_by_id(id):
+    pet = Pet.query.filter(Pet.id == id).first()
+
+    if pet:
+        response_body = f'<p>{pet.name} {pet.species}</p>'
+        response_status = 200
+    else:
+        response_body = f'<p>Pet {id} not found</p>'
+        response_status = 404
+
+    response = make_response(response_body, response_status)
+    return response
+
+
+@app.route('/species/<string:species>')
+def pet_by_species(species):
+    pets = Pet.query.filter_by(species=species).all()
+
+    size = len(pets)  # all() returns a list so we can get length
+    response_body = f'<h2>There are {size} {species}s</h2>'
+    for pet in pets:
+        response_body += f'<p>{pet.name}</p>'
+    response = make_response(response_body, 200)
+    return response
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
